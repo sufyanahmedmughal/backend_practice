52324b955c6c923daa40240f8e6d689efa7bb6fb
sufyanahmedmughal <sufyanahmedmughal01@gmail.com>
2026-03-15 21:06:54 +0500

    add 2 more get requests to test the concept of 'get'

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
+app.get('/login', (req, res) => {
+    res.send("You Can Login Here!");
+});
+
+app.get('/register', (req, res) => {
+    res.send("You Can Register Here!");
+});
+
 app.listen(port, () => {
     console.log("Listening on Port: " + port);
 });
